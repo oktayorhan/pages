d5ca713fa580cf7064812ae869f121a3e3ab6348
oktayorhan <140408160+oktayorhan@users.noreply.github.com>
2023-12-10 07:01:19 +0000

    index.js_Firebase/app

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom';
+import { initializeApp } from "firebase/app"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
